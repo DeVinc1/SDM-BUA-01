a422afff929758fad49c0babd5b43a5a1938f476
DeVinc1 <marcos.vinicius.dev.2004@gmail.com>
2025-03-18 11:06:44 -0300

    [feat] Trabalhar com Funções

---
+++ funcoes.js
@@ -0,0 +1,14 @@
+function ola(nome) {
+  return `Olá, ${nome}!`;
+}
+
+console.log(ola("Lewis Hamilton"));
+
+const soma = function(a, b) {
+  return a + b;
+}
+
+console.log(soma(44, 81));
+
+const multiplicacao = (a, b) => a * b;
+console.log(multiplicacao(10, 5));
